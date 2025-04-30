59a7d0223aaa9ad5aa79728ef0a818cf0469b7c4
Louis L <louis.luu92@gmail.com>
2025-05-01 07:33:29 +1000

    Fetch user's expiring documents

--- src/client.ts
+++ src/client.ts
@@ -19,15 +19,14 @@ export const authClient = {
   logout: () => api.post("/logout"),
 };
 
-type Document = {
+export type Document = {
   id: number;
   name: string;
   expires_at: string;
 };
 export const documentsClient = {
   getDocuments: () => api.get<{ data: Document[] }>("/api/documents"),
-  getDocument: (id: number) =>
-    api.get<{ data: Document }>(`/api/documents/${id}`),
+  getDocument: (id: number) => api.get<{ data: Document }>(`/api/documents/${id}`),
   archiveDocument: (id: number) => api.post(`/api/documents/${id}/archive`),
   createDocument: (id: number) => api.post(`/api/documents/${id}`),
 };
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -15,17 +15,27 @@
           </div>
           <div class="mt-8 flow-root">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div
-                class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
-              >
-                <table class="min-w-full divide-y divide-gray-300">
-                  <thead>
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table class="min-w-full text-left">
+                  <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
-                      <!--  -->
+                      <th class="px-6 py-3">ID</th>
+                      <th class="px-6 py-3">Name</th>
+                      <th class="px-6 py-3">Expiry</th>
+                      <th class="px-6 py-3"></th>
                     </tr>
                   </thead>
                   <tbody class="divide-y divide-gray-200">
-                    <!--  -->
+                    <tr
+                      v-for="document in userDocuments"
+                      :key="document.id"
+                      class="bg-white border-b border-gray-200"
+                    >
+                      <td class="px-6 py-4">{{ document.id }}</td>
+                      <td class="px-6 py-4">{{ document.name }}</td>
+                      <td class="px-6 py-4">{{ document.expires_at }}</td>
+                      <td class="px-6 py-4"></td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
@@ -38,5 +48,14 @@
 </template>
 
 <script setup lang="ts">
-//
+import { onMounted, ref } from 'vue'
+import { documentsClient, Document } from '../client.ts'
+
+const userDocuments = ref<Document[]>([])
+
+onMounted(async () => {
+  const response = await documentsClient.getDocuments()
+
+  userDocuments.value = response.data.data
+})
 </script>
